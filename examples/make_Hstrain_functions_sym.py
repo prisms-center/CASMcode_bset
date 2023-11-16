3bdcae148a8e28813ad6e21d77ac7adc29fb914d
jeremiah <jeremiahthomas@ucsb.edu>
2023-11-16 15:10:37 -0800

    update example to match existing

--- examples/make_Hstrain_functions_sym.py
+++ examples/make_Hstrain_functions_sym.py
@@ -1,9 +1,10 @@
-import numpy as np
-
+import libcasm.configuration as casmconfig
 import libcasm.xtal as xtal
 import libcasm.xtal.prims as xtal_prims
+import numpy as np
 
 from casm.bset import (
+    PolynomialFunction,
     Variable,
     make_global_dof_matrix_rep,
     make_symmetry_adapted_polynomials,
@@ -25,18 +26,28 @@ xtal_prim = xtal_prims.FCC(
     a=1.0,
     global_dof=[Hstrain_dof],
 )
+
+print("Prim:")
+print(xtal.pretty_json(xtal_prim.to_dict()))
+print()
+
+prim = casmconfig.Prim(xtal_prim)
 matrix_rep = make_global_dof_matrix_rep(
-    xtal_prim=xtal_prim,
-    dof_key="Hstrain",
+    prim=prim,
+    key="Hstrain",
 )
+
 variables = [
     Variable(i) for i in Hstrain_dof.axis_names()
 ]
+variable_subsets = [[0, 1, 2, 3, 4, 5]]
 basis_set = make_symmetry_adapted_polynomials(
     matrix_rep=matrix_rep,
     variables=variables,
+    variable_subsets=variable_subsets,
     min_poly_order=1,
     max_poly_order=3,
+    orthonormalize_in_place=True,
     verbose=True,
 )
 
@@ -49,6 +60,14 @@ print("Symmetry adapted polynomials functions:")
 for i, f in enumerate(basis_set):
     print(f"~~~ order: {f.order()}, function_index: {i} ~~~")
     f._basic_print()
+    
     print('Latex formula:')
     f._latex_print(variables)
+
+    if False:
+        data = f.to_dict()
+        print(xtal.pretty_json(data))
+        f_check = PolynomialFunction.from_dict(data)
+        assert f == f_check
+
     print()
